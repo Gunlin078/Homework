85926143897e794b240a67758eea5420eed27c2a
Gunlin <r.a.ft@mail.ru>
2026-09-02 21:31:01 +0200

    Плохое решение

--- Homework.cpp
+++ Homework.cpp
@@ -1,6 +1,18 @@
 ﻿#include <iostream>
 #include <string>
 
+bool findAndRemove(std::string& str, const std::string& to_remove) {
+
+    size_t pos = str.find(to_remove);
+
+    if (pos == std::string::npos) {
+        return false;
+    }
+
+    str.erase(pos, to_remove.length());
+    return true;
+}
+
 int main()
 {
     std::string allowed = "{}()[]";
@@ -8,16 +20,21 @@ int main()
 
     std::cout << "Введите строку из {}()[]\n";
     std::getline(std::cin, str);
+
     if (str.find_first_not_of(allowed) != std::string::npos) 
     {
         std::cout << "Неразрешённые символы\n";
     }
-    else if (true) 
-    {
-        std::cout << "Строка не существует\n";
-    }
-    else
-    {
-        std::cout << "Строка существует\n";
+    else {
+        while (true) {
+            bool isFind = false;
+            if (findAndRemove(str, "{}"))  isFind = true; 
+            if (findAndRemove(str, "()"))  isFind = true;
+            if (findAndRemove(str, "[]"))  isFind = true;
+            
+            if (str.empty())        std::cout << "Вс1 хорошо\n"; break;
+
+            if (isFind == false)    std::cout << "Вс1 плохо\n";  break;
+        }
     }
 }
