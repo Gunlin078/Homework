ccd51d2348c02004adbb9f01138c000469dee836
Gunlin <r.a.ft@mail.ru>
2026-09-12 19:31:56 +0200

    Рабочий номер 3

--- Homework.cpp
+++ Homework.cpp
@@ -1,35 +1,22 @@
-﻿#include <iostream>
-#include <numeric>
+﻿#include <algorithm>
+#include <iostream>
 #include <vector>
-//3k*5l*7m = x
-long long fastPow(long long base, int exp) {
-    long long res = 1;
-    while (exp > 0) {
-        if (exp & 1) res *= base;  
 
-        if (exp == 1) break;
+#define ULLONG unsigned long long
 
-        base *= base;            
-        exp >>= 1;              
-    }
-    return res;
-}
 int main() {
     setlocale(LC_ALL, "Russian");
 
     int x = 0;
-    std::vector<int> results;
+    std::vector<ULLONG> results;
     std::cin >> x;
 
-    //std::vector<int> xi(x);
-    //std::iota(xi.begin(), xi.end(), 0);
 
-
-    int p3 = 1;
+    ULLONG p3 = 1;
     while (p3<=x) {
-        int p5 = 1;
+        ULLONG p5 = 1;
         while (p3*p5 <= x) {
-            int p7 = 1;
+            ULLONG p7 = 1;
             while (p3 * p5 * p7 <= x) {
                 results.push_back(p3 * p5 * p7);
                 p7 *= 7;
@@ -38,7 +25,9 @@ int main() {
         }
         p3 *= 3;
     }
-    for (int i : results) {
+    std::sort(results.begin(), results.end());
+
+    for (ULLONG i : results) {
         std::cout << i << ", ";
     }
     
