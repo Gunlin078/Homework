dc17a7647c66756941928e7aaa78d7bfa3a6edb3
Gunlin <r.a.ft@mail.ru>
2026-09-14 22:37:19 +0200

    Вспомнил, что я на шарпах работать буду

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                string? input = Console.ReadLine()?.Replace(" ", "");
+
+                if (string.IsNullOrWhiteSpace(input))   break; 
+
+
+            }
+        }
+    }
+}
