02295a45a8f5c9873dc3b3723f900d9d29b98d2e
Gunlin <r.a.ft@mail.ru>
2026-09-18 16:22:56 +0200

    Пк

--- Program.cs
+++ Program.cs
@@ -2,6 +2,18 @@
 {
     internal class Program
     {        
+        public static bool HasExtraCharacters(string input)
+        {
+            ReadOnlySpan<char> span = input.AsSpan();
+
+            for (int i = 0; i < span.Length; i++)
+            {
+                char c = span[i];
+                if (c != ' ' && (c < '0' || c > '9'))   return true;
+            }
+            return false;
+        }
+
 		static void Main(string[] args)
         {
             Console.WriteLine("Enter numbers-—and only numbers—-separated by spaces");
@@ -12,18 +24,22 @@
                 if (string.IsNullOrWhiteSpace(input))   break;
                 List<int> numbers = [.. input
                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(s => int.TryParse(s))];                                 
+                    .Select(s => int.Parse(s))];                                 
             }
         }
     }
     class CombSorter 
     {
         private static long _step;
+        private static long _currentNumber;
         private static double _reductionFactor = 1.247f;
         
         static void CombSort(List<int> numbers)
         {
-            _step = (long)Math.Round(numbers.Count / _reductionFactor, MidpointRounding.AwayFromZero);
+            while (true){
+                _step = (long)Math.Round(numbers.Count / _reductionFactor, MidpointRounding.AwayFromZero);
+                
+            }
         }
     }
 }
