8afd1bedaf8934b1f7f188cda90267a0d2a881bf
Gunlin <r.a.ft@mail.ru>
2026-09-21 23:59:54 +0200

    Пк

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@
                 string? input = Console.ReadLine();
 
                 if (string.IsNullOrWhiteSpace(input))   break;
-                List<int> numbers= CombSorter.CombSort(input);
+                LinkedList<int> numbers= Sorter.InsertionSort(input);
 
                 foreach (var n in numbers)
                 {
@@ -20,35 +20,21 @@
             }
         }
     }
-    public static class CombSorter 
+    public static class Sorter 
     {
-        public static List<int> CombSort(string input)
+        public static LinkedList<int> InsertionSort(string input)
         {
-            List<int> numbers = [.. input
+            LinkedList<int> numbers = [.. input
                     .Split(' ', StringSplitOptions.RemoveEmptyEntries)
                     .Where( s => !HasExtraCharacters(s))
                     .Select(s => int.Parse(s))]; 
-            double _reductionFactor = 1.247;
             bool _proceed = true;
-
-            int _step = numbers.Count;
             
-            while (_step > 1 || _proceed){
-                _step = (int)(_step / _reductionFactor);
-                _proceed = false;
-                if (_step < 1)
+            while (_proceed){
+                for (int i = 1; i + 1 < numbers.Count; i++)
                 {
-                    _step = 1;
-                }
-
-                for ( int i = 0; i + _step < numbers.Count; i++)
-                {
-                    if (numbers[i] > numbers[i+_step]) 
-                    {
-                        _proceed = numbers.Swap(i, i+_step); 
-                    }
-                }
-                
+                    if
+                }    
             }
             return numbers;
         }
@@ -66,7 +52,7 @@
                 char c = span[i];
 
                 if (i == 0 && c == '-') continue;
-                
+
                 if (c != ' ' && (c < '0' || c > '9'))
                 {
                     return true;
