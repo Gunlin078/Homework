41964b69b10452760fb0e2a9a234b94fc03e33d4
Gunlin <r.a.ft@mail.ru>
2026-09-21 22:58:00 +0200

    Номер 4

--- Program.cs
+++ Program.cs
@@ -2,18 +2,6 @@
 {
     internal class Program
     {        
-        public static bool HasExtraCharacters(string input)
-        {
-            ReadOnlySpan<char> span = input.AsSpan();
-
-            for (int i = 0; i < span.Length; i++)
-            {
-                char c = span[i];
-                if (c != ' ' && (c < '0' || c > '9'))   return true;
-            }
-            return false;
-        }
-
 		static void Main(string[] args)
         {
             Console.WriteLine("Enter numbers-—and only numbers—-separated by spaces");
@@ -22,32 +10,70 @@
                 string? input = Console.ReadLine();
 
                 if (string.IsNullOrWhiteSpace(input))   break;
-                List<int> numbers = [.. input
-                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                    .Select(s => int.Parse(s))];                                 
+                List<int> numbers= CombSorter.CombSort(input);
+
+                foreach (var n in numbers)
+                {
+                    Console.Write(n + " "); 
+                }
+                Console.WriteLine();
             }
         }
     }
     public static class CombSorter 
     {
+        public static List<int> CombSort(string input)
+        {
+            List<int> numbers = [.. input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Where( s => !HasExtraCharacters(s))
+                    .Select(s => int.Parse(s))]; 
+            double _reductionFactor = 1.247;
+            bool _proceed = true;
 
-        public static void Swap<T>(this List<T> list, int index1, int index2)
+            int _step = numbers.Count;
+            
+            while (_step > 1 || _proceed){
+                _step = (int)(_step / _reductionFactor);
+                _proceed = false;
+                if (_step < 1)
+                {
+                    _step = 1;
+                }
+
+                for ( int i = 0; i + _step < numbers.Count; i++)
+                {
+                    if (numbers[i] > numbers[i+_step]) 
+                    {
+                        _proceed = numbers.Swap(i, i+_step); 
+                    }
+                }
+                
+            }
+            return numbers;
+        }
+        private static bool Swap<T>(this List<T> list, int index1, int index2)
         {
             (list[index1], list[index2]) = (list[index2], list[index1]);
+            return true;
         }
-        static void CombSort(List<int> numbers)
+        public static bool HasExtraCharacters(string input)
         {
-            int _step;
-            double _reductionFactor = 1.247;
+            ReadOnlySpan<char> span = input.AsSpan();
 
-            while (true){
-                _step = (int)Math.Round(numbers.Count / _reductionFactor, MidpointRounding.AwayFromZero);
+            for (int i = 0; i < span.Length; i++)
+            {
+                char c = span[i];
 
-                for ( int i = 0; i<numbers.Count - 2; i++)
+                if (i == 0 && c == '-') continue;
+                
+                if (c != ' ' && (c < '0' || c > '9'))
                 {
-                    if (numbers[i] > numbers[i+_step]) {}
+                    return true;
                 }
             }
+
+            return false;
         }
     }
 }
