6c64892a3ad49d689ae3a55ae8af7e9c3e7b0771
Gunlin <r.a.ft@mail.ru>
2026-09-25 20:13:49 +0200

    Ноут

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Homework
+﻿using System.Globalization;
+
+namespace Homework
 {
     internal class Program
     {        
@@ -29,8 +31,21 @@
                     .Where( s => !HasExtraCharacters(s))
                     .Select(int.Parse)]; 
             
-
-            
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                int initial = i+1;
+                int min = numbers[initial];
+                int nMin = initial;
+                for (; initial < numbers.Count; initial++)
+                {
+                    if (numbers[initial] < numbers[nMin])
+                    {
+                        min = numbers[initial];
+                        nMin = initial;
+                    }
+                }
+                numbers.Swap(i, nMin);
+            }
             return numbers;
         }
         private static bool Swap<T>(this List<T> list, int index1, int index2)
